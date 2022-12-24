d524471d4f9ca6bafcfe40f420a14d7b19f30251
Ernest Micklei <ernest.micklei@gmail.com>
2022-12-24 08:45:07 +0100

    add filter to iterator

--- page_iterator.go
+++ page_iterator.go
@@ -1,32 +1,34 @@
 package simone
 
 import (
+	"log"
 	"strconv"
 
 	"github.com/dop251/goja"
 )
 
 type PagingState struct {
-	NextPageToken string
-	PageSize      int
-	HasMorePages  bool
+	nextPageToken string
+	hasMorePages  bool
 	nextPageError error
 }
 
-func (p PagingState) PageIndex() int {
-	index, _ := strconv.Atoi(p.NextPageToken)
+func (p PagingState) NextPageIndex() int {
+	index, _ := strconv.Atoi(p.nextPageToken)
 	return index
 }
 
+func (p PagingState) NextPageToken() string { return p.nextPageToken }
+
 func (p PagingState) WithNextPageToken(token string) PagingState {
-	p.NextPageToken = token
-	p.HasMorePages = token != ""
+	p.nextPageToken = token
+	p.hasMorePages = token != ""
 	return p
 }
 
 func (p PagingState) WithPageIndex(index int, done bool) PagingState {
-	p.NextPageToken = strconv.Itoa(index)
-	p.HasMorePages = !done
+	p.nextPageToken = strconv.Itoa(index)
+	p.hasMorePages = !done
 	return p
 }
 
@@ -47,9 +49,9 @@ type PageIterator[T any] struct {
 	nexter   NextPageFunc[T]
 }
 
-func NewIterator[T any](pageSize int, nexter NextPageFunc[T]) *PageIterator[T] {
+func NewPagingIterator[T any](nexter NextPageFunc[T]) *PageIterator[T] {
 	return &PageIterator[T]{
-		State:  PagingState{PageSize: pageSize, HasMorePages: true},
+		State:  PagingState{hasMorePages: true},
 		nexter: nexter,
 	}
 }
@@ -64,8 +66,20 @@ func (i *PageIterator[T]) Map(block func(each T) any) (list []any) {
 	return
 }
 
-func (i *PageIterator[T]) NextPage(count ...int) []T {
-	if i.State.HasMorePages {
+// Filter implements JS filter function
+func (i *PageIterator[T]) Filter(block func(each T) bool) (list []any) {
+	for i.HasNextPage() {
+		for _, each := range i.NextPage() {
+			if block(each) {
+				list = append(list, each)
+			}
+		}
+	}
+	return
+}
+
+func (i *PageIterator[T]) NextPage() []T {
+	if i.State.hasMorePages {
 		page, nextState := i.nexter(i.State)
 		i.State = nextState
 		return page
@@ -74,7 +88,7 @@ func (i *PageIterator[T]) NextPage(count ...int) []T {
 }
 
 func (i *PageIterator[T]) HasNextPage() bool {
-	return i.State.nextPageError == nil && i.State.HasMorePages
+	return i.State.nextPageError == nil && i.State.hasMorePages
 }
 
 // ToProxy returns a JS proxy that dispatching collection functions to the iterator
@@ -82,8 +96,16 @@ func (i *PageIterator[T]) ToProxy(vm *goja.Runtime) goja.Proxy {
 	obj := vm.NewObject()
 	return vm.NewProxy(obj, &goja.ProxyTrapConfig{
 		Get: func(target *goja.Object, property string, receiver goja.Value) (value goja.Value) {
-			// fmt.Println("property:", property)
-			return vm.ToValue(i.Map)
+			// https://medium.com/@mandeepkaur1/a-list-of-javascript-array-methods-145d09dd19a0
+			switch property {
+			case "map":
+				return vm.ToValue(i.Map)
+			case "filter":
+				return vm.ToValue(i.Filter)
+			default:
+				log.Println("[simone.PageIterator] error: no such property:", property)
+				return goja.Null()
+			}
 		},
 		// GetIdx: func(target *goja.Object, property int, receiver goja.Value) (value goja.Value) {
 		// 	return s.vm.ToValue(1)
---
+++ page_iterator_test.go
@@ -0,0 +1,47 @@
+package simone
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/dop251/goja"
+)
+
+func createListProxy(vm *goja.Runtime) goja.Proxy {
+	it := NewPagingIterator(func(input PagingState) ([]int, PagingState) {
+		list := []int{1, 2, 3, 4, 5}
+		index := input.NextPageIndex()
+		end := index + 2
+		if end > len(list) {
+			end = len(list)
+		}
+		return list[index:end], input.WithPageIndex(end, end == len(list))
+	})
+	return it.ToProxy(vm)
+}
+
+func TestIterateMap(t *testing.T) {
+	vm := goja.New()
+	vm.Set("l", createListProxy(vm))
+	_, err := vm.RunString(`r = l.map((each)=>each*each)`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	gr := vm.Get("r").Export()
+	if got, want := gr, []int{1, 4, 9, 16, 25}; reflect.DeepEqual(got, want) {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
+}
+
+func TestIterateFilter(t *testing.T) {
+	vm := goja.New()
+	vm.Set("l", createListProxy(vm))
+	_, err := vm.RunString(`r = l.filter((each)=>(each%2) == 0)`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	gr := vm.Get("r").Export()
+	if got, want := gr, []int{2, 4}; reflect.DeepEqual(got, want) {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
+}
