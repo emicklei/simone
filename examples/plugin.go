8ba2067f2dfc6283fd857d0d489eda9263d2c119
Ernest Micklei <ernest.micklei@gmail.com>
2023-01-12 17:59:18 +0100

    add thing test

--- examples/plugin.go
+++ examples/plugin.go
@@ -14,3 +14,15 @@ func (d *Demo) Init(vm *goja.Runtime) error { return nil }
 
 // demo.time()
 func (d *Demo) Time() time.Time { return time.Now() }
+
+type Thing struct {
+	Name string
+	When time.Time
+}
+
+func (d *Demo) Thing() Thing {
+	return Thing{
+		Name: "some",
+		When: time.Now(),
+	}
+}
